dfd402594579facb61fb9675dfffe6e90f4e59c0
Gökhan Arlı <gokhan@sylow.net>
2014-05-17 08:57:40 +0300

    Use right syntax to capture callback data

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -3,20 +3,21 @@ class StripeController < ApplicationController
   
   def webhook
     event = JSON.parse(request.body.read)
-
+    customer_token = event.fetch("data").fetch("card").fetch("customer")
+    notification_type = event.fetch("type")
     # Transaction history
-    SubscriptionHistory.create({
-                                 stripe_customer_token: event.data.card.customer,
-                                 data: request.body.read,
-                                 notification_type: event.type
-                               }
-                               )    
+    SubscriptionHistory.create( {
+                                  stripe_customer_token: customer_token,
+                                  data: request.body.read,
+                                  notification_type: notification_type
+                                }
+                              )    
                                
     # Does this client has a subscription with us?
-    if subscription = Subscription.where(stripe_customer_token: event.data.card.customer).first      
-      if event.type == "charge.failed"        # if something goes wrong let our client know it
+    if subscription = Subscription.where(stripe_customer_token: customer_token).first      
+      if notification_type == "charge.failed"        # if something goes wrong let our client know it
         UserMailer.payment_failure(subscription.user).deliver    
-      elsif event.type == "charge.succeeded"  # Activate this client if charge is successful
+      elsif notification_type == "charge.succeeded"  # Activate this client if charge is successful
         subscription.update_attribute(:active, true)    
       end
     end        
