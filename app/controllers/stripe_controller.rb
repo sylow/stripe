a30dc2deccbb0b36ec9f930fab1a25d7eb33bc6a
Gökhan Arlı <gokhan@sylow.net>
2014-05-22 21:59:05 +0300

    Fix webhook using the right model

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -1,6 +1,6 @@
 class StripeController < ApplicationController
   protect_from_forgery except: :webhook
-  
+
   def webhook
     event = JSON.parse(request.body.read)
     customer_token = event.fetch("data").fetch("object").fetch("customer")
@@ -11,18 +11,18 @@ class StripeController < ApplicationController
                                   data: request.body.read,
                                   notification_type: notification_type
                                 }
-                              )    
-                               
+                              )
+
     # Does this client has a subscription with us?
-    if subscription = Subscription.where(stripe_customer_token: customer_token).first      
+    if user = User.where(stripe_customer_token: customer_token).first
       if notification_type == "invoice.payment_failed"        # if something goes wrong let our client know it
-        UserMailer.payment_failure(subscription.user).deliver    
-        subscription.update_attribute(:active, false)    
+        UserMailer.payment_failure(user).deliver
+        user.subscription.update_attribute(:active, false)
       elsif notification_type == "invoice.payment_succeeded"  # Activate this client if charge is successful
-        subscription.update_attribute(:active, true)    
+        user.subscription.update_attribute(:active, true)
       end
-    end        
-    
+    end
+
     head :ok # stripe requires us to return ok
   end
 end
